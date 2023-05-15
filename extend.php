3b2e1c2c68f7dd979f6521e960ad6ab23eb4acee
William <nearata@protonmail.com>
2023-05-15 15:10:40 +0200

    fix

--- extend.php
+++ extend.php
@@ -1,12 +1,13 @@
 <?php
 
 use Flarum\Extend;
-use Flarum\User\Event\Registered;
+use Flarum\User\Event\Saving;
 
 return [
     (new Extend\Event)
-        ->listen(Registered::class, function (Registered $event) {
-            $event->user->activate();
-            $event->user->save();
+        ->listen(Saving::class, function (Saving $event) {
+            if ($event->actor->isGuest()) {
+                $event->user->activate();
+            }
         })
 ];
