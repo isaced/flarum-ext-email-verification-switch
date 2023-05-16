3a04b3bed8d3ff8893aa4351f784fa69b1827a24
William <nearata@protonmail.com>
2023-05-16 17:57:38 +0200

    we can actually check for user existence

--- extend.php
+++ extend.php
@@ -6,7 +6,7 @@ use Flarum\User\Event\Saving;
 return [
     (new Extend\Event)
         ->listen(Saving::class, function (Saving $event) {
-            if ($event->actor->isGuest()) {
+            if (!$event->user->exists) {
                 $event->user->activate();
             }
         })
